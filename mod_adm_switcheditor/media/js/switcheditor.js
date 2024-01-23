783fec972bf17e6e9ca0edcfb563de46a3aa57c0
pmleconte <pascal.leconte@nordnet.fr>
2024-01-23 17:56:46 +0100

    2.2.6
    SP Page Builder conflict : remove form tag

--- mod_adm_switcheditor/media/js/switcheditor.js
+++ mod_adm_switcheditor/media/js/switcheditor.js
@@ -1,18 +1,16 @@
 /**
  * @package    Switch Editor
  * @subpackage mod_switcheditor
- * @copyright  Copyright (C) 2023 ConseilGouz. All rights reserved.
+ * @copyright  Copyright (C) 2024 ConseilGouz. All rights reserved.
  * From anything-digital.com Switch Editor
- * @license    GNU/GPLv2
+ * @license    GNU/GPLv3
  */
 
 document.addEventListener("DOMContentLoaded", function(){
-options = Joomla.getOptions('mod_switcheditor');
 
 const els = document.querySelectorAll('.adEditor');	
 for (let i = 0; i < els.length; i++) {
 	els[i].addEventListener('change', function (ev) {
-			var f = ev.srcElement.closest('form');
 			var csrf = Joomla.getOptions("csrf.token", "");
 				url = "?"+csrf+"=1&option=com_ajax&module=switcheditor&adEditor="+ev.srcElement.value+"&task=switch&format=json";
 				Joomla.request({
