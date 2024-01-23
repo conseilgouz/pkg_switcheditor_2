8c9c9c8e6f220948d2e0544e46359104210de583
pmleconte <pascal.leconte@nordnet.fr>
2024-01-23 18:09:05 +0100

    2.2.5
    reverses 2.2.6

--- mod_site_switcheditor/tmpl/default.php
+++ mod_site_switcheditor/tmpl/default.php
@@ -2,9 +2,9 @@
 /**
  * @package    Switch Editor
  * @subpackage mod_switcheditor
- * @copyright  Copyright (C) 2024 ConseilGouz. All rights reserved.
+ * @copyright  Copyright (C) 2021 ConseilGouz. All rights reserved.
  * From anything-digital.com Switch Editor
- * @license    GNU/GPLv3
+ * @license    GNU/GPLv2
  */
 // no direct access
 defined('_JEXEC') or die;
@@ -13,5 +13,9 @@ use Joomla\CMS\HTML\HTMLHelper;
 if (($app->input->get('option') == 'com_fields') && ($app->input->get('view') == 'field') && ($app->input->get('layout') == 'edit')) return;
 ?>
 <div class="adEditorFormBox btn-group">
+	<form name="adEditorFormSite">
 		<?php echo str_replace(' id="adEditor"', '', HTMLHelper::_('select.genericlist', $options,'adEditor', ' class="adEditor chzn-done" data-chosen="done"','element', 'name',$value)); ?>
+		<input type="hidden" name="task" value="switch" />
+		<?php echo HTMLHelper::_('form.token'); ?>
+	</form>
 </div>
